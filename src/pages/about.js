5c0358084b06922f4ca91211cb01167f85762e93
horford <harryorford1@googlemail.com>
2020-06-07 14:44:48 +0100

    change sub image about

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,35 +16,35 @@ const AboutPage = ({ data }) => (
             <h1 className="text-3xl leading-tight font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               About me
             </h1>
-
-            <div className="mt-4 leading-loose">
-            My name is Harry and I'm a graduate from Falmouth University where I studied a BA Animation & 
-            Visual Effects degree and I'm currently working towards being an FX artist. 
-            My software specialities are as follows; Houdini, Maya, Renderman, Arnold, Nuke.
-              <br />
-              <br />
-              I'm also currently learning C++ and Python basics. I have a strong background in live action compositing. 
-              I've also had the privilege of being able to use a wide range of different cameras at university which includes the Blackmagic cameras and Red Cinema cameras. 
-              This has allowed me to collect a wide range of stock footage for personal use in the future.
-              <br />
-              <br />
-              I'd be more than happy to hear from you:
-              <br />
-              <a
-                href="mailto:harryorford1@gmail.com"
-                className="border-b border-gray-500 hover:border-blue-600 hover:text-blue-600"
-              >
-                harryorford1@gmail.com
-              </a>
+            <br></br>
+              <div className="mt-4 leading-loose">
+              My name is Harry and I'm a graduate from Falmouth University where I studied a BA Animation & 
+              Visual Effects degree and I'm currently working towards being an FX artist. 
+              My software specialities are as follows; Houdini, Maya, Renderman, Arnold, Nuke.
+                <br />
+                <br />
+                I'm also currently learning C++ and Python basics. I have a strong background in live action compositing. 
+                I've also had the privilege of being able to use a wide range of different cameras at university which includes the Blackmagic cameras and Red Cinema cameras. 
+                This has allowed me to collect a wide range of stock footage for personal use in the future.
+                <br />
+                <br />
+                I'd be more than happy to hear from you:
+                <br />
+                <a
+                  href="mailto:harryorford1@gmail.com"
+                  className="border-b border-gray-500 hover:border-blue-600 hover:text-blue-600"
+                >
+                  harryorford1@gmail.com
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="w-full md:w-1/2 xl:w-2/5 md:pl-12">
-            <Img
-              fluid={data.harry.childImageSharp.fluid}
-              alt="John Doe"
-              className="rounded-md shadow-md"
-            />
-          </div>
+            <div className="w-full md:w-1/2 xl:w-2/5 md:pl-12">
+              <Img
+                fluid={data.harry.childImageSharp.fluid}
+                alt="Harry Orford"
+                className="rounded-md shadow-md"
+              />
+            </div>
         </div>
       </div>
     </div>
